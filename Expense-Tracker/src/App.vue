2e0d039e27e3ac75b7392c74127a0338c8979e35
Chelsea Pierre <chelseapierre82@yahoo.com>
2024-08-19 19:42:10 -0400

    implemented local storage for keeping added expenses and income in the expense tracker upon reload

--- Expense-Tracker/src/App.vue
+++ Expense-Tracker/src/App.vue
@@ -6,16 +6,20 @@
   import AddTransaction from './components/AddTransaction.vue';
   import {useToast} from 'vue-toastification';
 
-  import { ref, computed } from 'vue';
+  import { ref, computed, onMounted } from 'vue';
 
   const toast = useToast();
 
-  const transactions = ref ([
-        {id: 1, text: 'Flower', amount: -19.99},
-        {id: 2, text: 'Salary', amount: 299.97},
-        {id: 3, text: 'Book', amount: -10},
-        {id: 4, text: 'Camera', amount: -150},
-    ]);
+  const transactions = ref ([]);
+
+  onMounted (() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+    if (savedTransactions) {
+      transactions.value = savedTransactions;
+    }
+
+  });
 
   //get total
   const total = computed(() => {
@@ -51,19 +55,30 @@ const expenses = computed(() => {
       amount: transactionData.amount,
     });
 
-    //console.log(generateUniqueID())
+    saveTransactionsToLocalStorage();
+
     toast.success('Transaction added');
+    console.log('Transaction added with id of: ' + generateUniqueID());
   };
 
   //generate unique ID
-  const generateUniqueID = () =>{
+  const generateUniqueID = () => {
     return Math.floor(Math.random() * 1000000000);
   }
 
   //delete transaction
   const handleTransactionDeleted = (id) => {
    transactions.value = transactions.value.filter((transaction) => transaction.id !== id);
+
+   saveTransactionsToLocalStorage();
+   
    toast.success('Transaction deleted');
+   console.log("id deleted: " + id);
+  };
+
+  //save to local storage
+  const saveTransactionsToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value));
   };
 </script>
 
